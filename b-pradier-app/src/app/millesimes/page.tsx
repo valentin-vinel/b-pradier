738ea2f1db211ceef0500105be3997e66a9b86f7
valentin-vinel <valentinvnl84@gmail.com>
2025-10-16 08:25:34 +0200

    page catalogue

--- b-pradier-app/src/app/millesimes/page.tsx
+++ b-pradier-app/src/app/millesimes/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import Image from "next/image";
 import Header from "../../shared/Header";
-import bouteilles from "../../../public/millesimes/bouteilles.png"
-import bouteillesCopie from "../../../public/millesimes/bouteilles-Copie.png"
+import bouteilles from "../../../public/millesimes/bouteilles-Copie.png"
+// import heroImg from "../../../public/cadre-hero.png"
+// import cadre from "../../../public/cadre.png"
+import ardoise from "../../../public/millesimes/ardoise.png"
+import bouchon from "../../../public/bouchon.png"
 
 
 export default function Histoire() {
@@ -42,31 +45,32 @@ export default function Histoire() {
 
     return (
         <div className="min-h-screen flex flex-col max-w-[1200px] m-auto px-3 lg:px-0">
+            <div className="fixed z-[-1] w-full h-screen bg-total-cave opacity-60 left-0 top-0"></div>
             <Header />
 
             <main className="mb-6">
-                <h2 className="text-center font-bold mb-4 text-2xl">Nos 3 cuvées</h2>
-                <figure className="m-auto text-center mb-6">
-                    <Image src={bouteillesCopie} alt="Photo des bouteilles de nos 3 cuvées, non contractuelle." className="m-auto w-[280px] img-shadow rounded-xl"></Image>
-                    <figcaption className="text-base mt-4">Photo non contractuelle.</figcaption>
+                <h2 className="text-center font-bold mb-2 text-3xl title-millesime">Nos trois cuvées</h2>
+                <figure className="m-auto text-center mb-8 ">
+                    <Image src={bouteilles} alt="Photo des bouteilles de nos 3 cuvées, non contractuelle." className="m-auto w-[240px] shadow-2xl border-bouteille relative"></Image>
+                    <figcaption className="text-base mt-2">Photo non contractuelle.</figcaption>
                 </figure>
 
 
-                <div className="grid grid-cols-3 gap-2 m-auto text-center w-fit">
-                    <h3 className="text-2xl">Grande Réserve</h3>
-                    <h3 className="text-2xl">Cuvée Sylviane</h3>
-                    <h3 className="text-2xl">Orée des Collines</h3>
-                    {/* {millesimes.map((element, i) => (
-                        <article key={i} className="w-[250px] h-[250px] bg-cave flex justify-center">
-                            <div className="w-[210px] h-[200px] bg-[#E7DFC9]/95 border-article rounded-xl m-auto flex flex-col justify-around p-2">
-                                <h4 className="text-4xl font-bold">{element.annee}</h4>
-                                <p className="text-base">Quantité disponible: {element.stock}</p>
-                                <button className="bg-gray-500 p-2 text-base text-white rounded-xl hover:font-bold hover:cursor-pointer">Ajouter à ma commande</button>
-                            </div>
-                        </article>
-                    ))} */}
+                <div className="grid grid-cols-3 gap-6 m-auto text-center w-fit">
+                    <figure className="flex flex-col justify-center items-center relative">
+                        <Image src={ardoise} alt="Image d'un cadre suspendu" className="max-w-[240px]"></Image>
+                        <h3 className="text-2xl text-white absolute h-fit m-auto translate-y-1/4 w-[140px] font-bold -rotate-6">GRANDE RÉSERVE</h3>
+                    </figure>
+                    <figure className="flex flex-col justify-center items-center relative">
+                        <Image src={ardoise} alt="Image d'un cadre suspendu" className="max-w-[240px]"></Image>
+                        <h3 className="text-2xl text-white absolute h-fit m-auto translate-y-1/4 w-[140px] font-bold -rotate-6">CUVÉE SYLVIANE</h3>
+                    </figure>
+                    <figure className="flex flex-col justify-center items-center relative">
+                        <Image src={ardoise} alt="Image d'un cadre suspendu" className="max-w-[240px]"></Image>
+                        <h3 className="text-2xl text-white absolute h-fit m-auto translate-y-1/4 w-[140px] font-bold -rotate-6">L'ORÉE DES COLLINES</h3>
+                    </figure>
 
-                    {millesimes.map((element, i) => (
+                    {/* {millesimes.map((element, i) => (
                         <article key={i} className="w-[250px] h-[250px] bg-cave-b flex justify-center">
                             <div className="m-auto flex flex-col justify-around gap-4">
                                 <h4 className="text-4xl font-bold">{element.annee}</h4>
@@ -74,7 +78,28 @@ export default function Histoire() {
                                 <button className="bg-[#aaa28f] border-article py-1 px-4 text-base text-white rounded-xl hover:font-bold hover:cursor-pointer hover:text-[#6b1e1e]">Ajouter à ma commande</button>
                             </div>
                         </article>
+                    ))} */}
+
+                    {millesimes.map((element, i) => (
+                        <article key={i} className="flex flex-col gap-4 mb-6 items-center">
+                            <figure className="flex flex-col justify-center items-center relative">
+                                <Image src={bouchon} alt="Image d'un bouchon de liège" className="max-w-[130px]"></Image>
+                                <figcaption className="lgd-bouchon text-3xl absolute h-fit m-auto translate-x-4 w-[140px] font-bold">{element.annee}</figcaption>
+                            </figure>
+                            <button className="bg-[#D4BFA6] border-2  text-base hover: hover:cursor-pointer hover:text-[#6b1e1e] relative w-[210px]">
+                                <span className="w-[3px] h-[3px] bg-black absolute top-1 left-1 rounded-xl" />
+                                <span className="w-[3px] h-[3px] bg-black absolute top-1 right-1 rounded-xl" />
+                                <span className="w-[3px] h-[3px] bg-black absolute bottom-1 left-1 rounded-xl" />
+                                <span className="w-[3px] h-[3px] bg-black absolute bottom-1 right-1 rounded-xl" />
+                                Ajouter à ma commande
+                            </button>
+                            {/* <div className="m-auto flex flex-col justify-around gap-4">
+                                <h4 className="text-4xl font-bold">{element.annee}</h4>
+                                <p className="text-base">Quantité disponible: {element.stock}</p>
+                            </div> */}
+                        </article>
                     ))}
+
                     
                         
                     
