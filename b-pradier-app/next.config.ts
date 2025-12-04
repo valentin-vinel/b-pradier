4a046e68367dca15e6ca9f4faba99a32091a5dcd
valentin-vinel <valentinvnl84@gmail.com>
2025-12-04 15:15:47 +0100

    MAJ Next

--- b-pradier-app/next.config.ts
+++ b-pradier-app/next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    images: {
-        domains: ['cdn.shopify.com'],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
