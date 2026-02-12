7da7ebd2d193cf8987a32e2dfb3b186f26619e50
valentin-vinel <valentinvnl84@gmail.com>
2026-02-12 10:11:54 +0100

    correction: décimal sur le prix total de l'offre

--- b-pradier-app/src/shared/OffreComponent.tsx
+++ b-pradier-app/src/shared/OffreComponent.tsx
@@ -84,7 +84,7 @@ export default function OffreComponent() {
 
           {/*  --- Ligne TOTAL --- */}
           <p className="text-center my-4">Vin rouge. 13,5% vol. 75 cl. Produit de France. Contient des sulfites.</p>
-          <p className="text-center my-4">Prix : <span className="line-through text-red">{product.variants?.edges[0].node.compareAtPriceV2?.amount} €</span> <span className="text-2xl font-bold">{product.variants?.edges[0].node.priceV2.amount} €</span></p>
+          <p className="text-center my-4">Prix : <span className="line-through text-red">{Number(product.variants?.edges[0].node.compareAtPriceV2?.amount).toFixed(2)} €</span> <span className="text-2xl font-bold">{Number(product.variants?.edges[0].node.priceV2.amount).toFixed(2)} €</span></p>
           <p className="text-center">Profitez de 10% de remise sur cette offre</p>
 
           {/* --- Bouton unique pour ajouter les 6 bouteilles --- */}
